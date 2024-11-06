b6d3f9f6f6eb7c0824748c21fdd1564c551fa898
BorisBanchev <boris.banchev@helsinki.com>
2024-11-06 16:47:22 +0200

    Tehtävä 1.10

--- Osa1/unicafe/src/App.jsx
+++ Osa1/unicafe/src/App.jsx
@@ -18,19 +18,12 @@ const Statistics = (props) => {
     return (
       <div>
         <h1>statistics</h1>
-        <p>
-          good {props.good}
-          <br />
-          neutral {props.neutral}
-          <br />
-          bad {props.bad}
-          <br />
-          all {props.total}
-          <br />
-          average {average}
-          <br />
-          positive {positive}%
-        </p>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={props.total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + " %"} />
       </div>
     );
   }
@@ -38,6 +31,14 @@ const Statistics = (props) => {
     <div>
       <h1>statistics</h1>
       <b>No feedback given</b>
+    </div>
+  );
+};
+
+const StatisticLine = (props) => {
+  return (
+    <div>
+      {props.text} {props.value}
     </div>
   );
 };
