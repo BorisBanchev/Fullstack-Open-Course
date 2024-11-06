11410e79bf80dc0b26c4e75f8eb79f448e8f62f0
BorisBanchev <boris.banchev@helsinki.com>
2024-11-06 20:09:57 +0200

    Tehtävä 1.13

---
+++ Osa1/anekdootit/src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const min = 0;
+  const max = anecdotes.length - 1;
+  const [selected, setSelected] = useState(
+    Math.floor(Math.random() * (max - min + 1)) + min
+  );
+  const [points, setPoints] = useState(new Uint8Array(anecdotes.length));
+
+  const setMorePoints = (index) => {
+    const copy = [...points];
+    copy[index] += 1;
+    setPoints(copy);
+  };
+
+  const setToSelected = () => {
+    const newAnecdote = Math.floor(Math.random() * (max - min + 1)) + min;
+    setSelected(newAnecdote);
+  };
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <br />
+      has {points[selected]} votes
+      <br />
+      <Button handleClick={() => setMorePoints(selected)} text="vote" />{" "}
+      <Button handleClick={setToSelected} text="new anecdote" />
+    </div>
+  );
+};
+
+export default App;
