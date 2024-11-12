9229e7f34ed563b8d2fd309c6f02567a16b0d7bf
BorisBanchev <boris.banchev@helsinki.com>
2024-11-12 21:28:54 +0200

    Tehtävä 2.6

---
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+const Person = ({ person }) => {
+  return <div>{person.name}</div>;
+};
+
+const Persons = ({ persons }) => {
+  const phones = persons;
+  return (
+    <div>
+      {phones.map((person, name) => (
+        <Person person={person} key={name} />
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Persons persons={persons} />
+    </div>
+  );
+};
+
+export default App;
