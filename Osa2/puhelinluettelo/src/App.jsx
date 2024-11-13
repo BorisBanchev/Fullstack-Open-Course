4b1605380ecfac727f61b375a36552b33e1c9f84
BorisBanchev <boris.banchev@helsinki.com>
2024-11-13 13:33:32 +0200

    Tehtävä 2.9

--- Osa2/puhelinluettelo/src/App.jsx
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -11,11 +11,13 @@ const Person = ({ person }) => {
   );
 };
 
-const Persons = ({ persons }) => {
-  const phones = persons;
+const Persons = ({ persons, filter }) => {
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
   return (
     <div>
-      {phones.map((person, name) => (
+      {filteredPersons.map((person, name) => (
         <Person person={person} key={name} />
       ))}
     </div>
@@ -24,11 +26,14 @@ const Persons = ({ persons }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1231244" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-
+  const [filterText, setFilterText] = useState("");
   const names = persons.map((person) => person.name);
 
   const addPerson = (event) => {
@@ -57,9 +62,18 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleFilterTextChange = (event) => {
+    setFilterText(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input value={filterText} onChange={handleFilterTextChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -72,7 +86,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <Persons persons={persons} filter={filterText} />
     </div>
   );
 };
