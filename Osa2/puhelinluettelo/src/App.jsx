371a714c68ffa23a658ea362a4e77a3022ff8bd7
BorisBanchev <boris.banchev@helsinki.com>
2024-11-14 14:33:17 +0200

    Tehtävä 2-11

--- Osa2/puhelinluettelo/src/App.jsx
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Person = ({ person }) => {
   return (
@@ -17,8 +18,8 @@ const Persons = ({ persons, filter }) => {
   );
   return (
     <div>
-      {filteredPersons.map((person, name) => (
-        <Person person={person} key={name} />
+      {filteredPersons.map((person, id) => (
+        <Person person={person} key={id} />
       ))}
     </div>
   );
@@ -56,15 +57,18 @@ const Personform = ({
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterText, setFilterText] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
   const names = persons.map((person) => person.name);
 
   const addPerson = (event) => {
@@ -78,6 +82,7 @@ const App = () => {
     const personObject = {
       name: newName,
       number: newNumber,
+      id: String(persons.length + 1),
     };
 
     setPersons(persons.concat(personObject));
