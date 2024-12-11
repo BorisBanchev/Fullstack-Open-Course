eb86e59fdb4c55def0ef11da6b2ba8a0245f3845
BorisBanchev <boris.banchev03@gmail.com>
2024-12-11 17:23:01 +0200

    Tehtävä 4.2

--- Osa4/bloglistApp/index.js
+++ Osa4/bloglistApp/index.js
@@ -1,49 +1,7 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const port = process.env.PORT;
-const url = process.env.MONGODB_URI;
+const app = require("./app");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-mongoose.set("strictQuery", false);
-
-const mongoUrl = mongoose.connect(url);
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-blogSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
