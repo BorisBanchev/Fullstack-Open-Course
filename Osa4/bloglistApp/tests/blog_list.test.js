0774587ba0301854203d1a1f7938721d11b3532c
BorisBanchev <boris.banchev03@gmail.com>
2024-12-12 15:54:04 +0200

    Tehtävä 4.4

--- Osa4/bloglistApp/tests/blog_list.test.js
+++ Osa4/bloglistApp/tests/blog_list.test.js
@@ -1,9 +1,53 @@
 const { test, describe } = require("node:test");
 const assert = require("assert");
 const listHelper = require("../utils/list_helper");
+const totalLikes = listHelper.totalLikes;
+
+const blogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+];
 
 test("dummy returns one", () => {
   const blogs = [];
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
+});
+
+describe("total likes", () => {
+  test("of empty list is zero", () => {
+    assert.strictEqual(totalLikes([]), 0);
+  });
+
+  test("when list has only one blog equals the likes of that", () => {
+    const singleBlog = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+    ];
+    assert.strictEqual(totalLikes(singleBlog), 5);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    assert.strictEqual(totalLikes(blogs), 12);
+  });
 });
