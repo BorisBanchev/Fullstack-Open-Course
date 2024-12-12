e1d20ae17fc4d0cb592d1ea12761b7ebbd3232cd
BorisBanchev <boris.banchev03@gmail.com>
2024-12-12 16:09:48 +0200

    Tehtävä 4.5

--- Osa4/bloglistApp/tests/blog_list.test.js
+++ Osa4/bloglistApp/tests/blog_list.test.js
@@ -2,6 +2,7 @@ const { test, describe } = require("node:test");
 const assert = require("assert");
 const listHelper = require("../utils/list_helper");
 const totalLikes = listHelper.totalLikes;
+const favoriteBlog = listHelper.favoriteBlog;
 
 const blogs = [
   {
@@ -18,6 +19,22 @@ const blogs = [
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
+    __v: 0,
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0,
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
     __v: 0,
   },
 ];
@@ -48,6 +65,16 @@ describe("total likes", () => {
   });
 
   test("of a bigger list is calculated right", () => {
-    assert.strictEqual(totalLikes(blogs), 12);
+    assert.strictEqual(totalLikes(blogs), 34);
+  });
+});
+
+describe("favorite blog", () => {
+  test("list with one returns that blog", () => {
+    assert.strictEqual(favoriteBlog([(singleBlogList = blogs[0])]), blogs[0]);
+  });
+
+  test("list with multiple blogs returns the one with most likes", () => {
+    assert.strictEqual(favoriteBlog(blogs), blogs[2]);
   });
 });
