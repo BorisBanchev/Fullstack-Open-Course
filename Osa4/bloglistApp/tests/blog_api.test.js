f4402cb9be8ff66999d752032406368942af359b
BorisBanchev <boris.banchev03@gmail.com>
2024-12-13 14:14:41 +0200

    Tehtävä 4.10

--- Osa4/bloglistApp/tests/blog_api.test.js
+++ Osa4/bloglistApp/tests/blog_api.test.js
@@ -1,5 +1,6 @@
 const { test, after, beforeEach } = require("node:test");
 const assert = require("assert");
+const describe = require("node:test").describe;
 const Blog = require("../models/blog");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
@@ -46,6 +47,29 @@ test("blogs have id field instead of _id", async () => {
   blogs.forEach((blog) => {
     assert.ok(blog.id);
     assert.strictEqual(blog._id, undefined);
+  });
+});
+
+describe("adding a new blog", () => {
+  test("a valid blog can be added", async () => {
+    const newBlog = {
+      title: "Title 3",
+      author: "Author 3",
+      url: "URL3.com",
+      likes: 3,
+    };
+
+    const postResponse = await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    newBlog.id = postResponse.body.id;
+    const response = await api.get("/api/blogs");
+
+    assert.strictEqual(response.body.length, initialBlogs.length + 1);
+    assert.deepStrictEqual(response.body[2], newBlog);
   });
 });
 
