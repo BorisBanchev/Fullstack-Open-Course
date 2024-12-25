204048c65bf062c0e1787a5803d0904259672102
BorisBanchev <boris.banchev03@gmail.com>
2024-12-25 16:10:16 +0200

    Tehtävä 5.7

--- Osa5/bloglist-frontend/src/components/Blog.jsx
+++ Osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    - {blog.title}, {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [showAllData, setShowAllData] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleVisibility = () => {
+    setShowAllData(!showAllData);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}, {blog.author}{" "}
+        <button onClick={toggleVisibility}>
+          {showAllData ? "hide" : "view"}
+        </button>
+      </div>
+      {showAllData && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
