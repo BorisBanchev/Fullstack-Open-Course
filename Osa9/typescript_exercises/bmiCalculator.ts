a0c9d4ed9e9a4bd5cf84f4272d9351b8c03a37a5
BorisBanchev <boris.banchev03@gmail.com>
2025-02-18 16:26:35 +0200

    Tehtävä 9.5

--- Osa9/typescript_exercises/bmiCalculator.ts
+++ Osa9/typescript_exercises/bmiCalculator.ts
@@ -1,4 +1,4 @@
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / (height / 100) ** 2;
   if (bmi < 18.5) {
     return "underweight";
@@ -26,13 +26,15 @@ const parseArgumentsForBmi = (
   return { height, weight };
 };
 
-try {
-  const { height, weight } = parseArgumentsForBmi(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  let errorMessage = "Something went wrong: ";
-  if (error instanceof Error) {
-    errorMessage += error.message;
+if (require.main === module) {
+  try {
+    const { height, weight } = parseArgumentsForBmi(process.argv);
+    console.log(calculateBmi(height, weight));
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong: ";
+    if (error instanceof Error) {
+      errorMessage += error.message;
+    }
+    console.log(errorMessage);
   }
-  console.log(errorMessage);
 }
