8b91ad897ee0cb9fae65e76e6f3eb776b5038ea1
BorisBanchev <boris.banchev03@gmail.com>
2025-02-19 15:39:42 +0200

    Tehtävä 9.8

---
+++ Osa9/Patientor-backend/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+const PORT = 3000;
+
+app.get("/api/ping", (_req, res) => {
+  res.send("pong");
+});
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
